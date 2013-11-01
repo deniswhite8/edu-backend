c185532a704beecc4b9d26234734d2d119738a4c
denis belov <deniswhite8@gmail.com>
2013-11-01 23:11:45 +0400

    fix tests

--- tests/ProductCollectionTest.php
+++ tests/ProductCollectionTest.php
@@ -5,101 +5,83 @@ require_once __DIR__ . '/../src/ProductCollection.php';
 
 class ProductCollectionTest extends PHPUnit_Framework_TestCase
 {
-    public function testGetProductsEquals1()
+    public function testGetProductsEquals()
     {
         $products = new ProductCollection([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar'])]);
         $this->assertEquals([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar'])], $products->getProducts());
     }
 
-    public function testGetSizeEquals1()
+    public function testGetSizeEquals()
     {
         $products = new ProductCollection([new Product([]), new Product([])]);
         $this->assertEquals(2, $products->getSize());
-    }
 
-    public function testGetSizeEquals2()
-    {
         $products = new ProductCollection([new Product([])]);
         $this->assertEquals(1, $products->getSize());
     }
 
-    public function testGetSizeWithLimitEquals1()
+    public function testGetSizeWithLimitEquals()
     {
         $products = new ProductCollection([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar']), new Product(['sku' => 'baz'])]);
         $products->limit(1);
         $this->assertEquals(1, $products->getSize());
-    }
 
-    public function testGetProductsWithLimitEquals1()
-    {
-        $products = new ProductCollection([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar']), new Product(['sku' => 'baz'])]);
-        $products->limit(1);
-        $this->assertEquals([new Product(['sku' => 'fuu'])], $products->getProducts());
-    }
-
-    public function testGetSizeWithLimitEquals2()
-    {
         $products = new ProductCollection([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar']), new Product(['sku' => 'baz'])]);
         $products->limit(2);
         $this->assertEquals(2, $products->getSize());
     }
 
-    public function testGetProductsWithLimitEquals2()
+    public function testGetProductsWithLimitEquals()
     {
+        $products = new ProductCollection([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar']), new Product(['sku' => 'baz'])]);
+        $products->limit(1);
+        $this->assertEquals([new Product(['sku' => 'fuu'])], $products->getProducts());
+
         $products = new ProductCollection([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar']), new Product(['sku' => 'baz'])]);
         $products->limit(2);
         $this->assertEquals([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar'])], $products->getProducts());
     }
 
-    public function testGetProductsWithOffsetEquals1()
+    public function testGetProductsWithOffsetEquals()
     {
         $products = new ProductCollection([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar']), new Product(['sku' => 'baz'])]);
         $products->offset(0);
         $this->assertEquals([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar']), new Product(['sku' => 'baz'])], $products->getProducts());
-    }
 
-    public function testGetProductsWithOffsetEquals2()
-    {
         $products = new ProductCollection([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar']), new Product(['sku' => 'baz'])]);
         $products->offset(1);
         $this->assertEquals([new Product(['sku' => 'bar']), new Product(['sku' => 'baz'])], $products->getProducts());
-    }
 
-    public function testGetProductsWithOffsetEquals3()
-    {
         $products = new ProductCollection([new Product(['sku' => 'fuu']), new Product(['sku' => 'bar']), new Product(['sku' => 'baz'])]);
         $products->offset(2);
         $this->assertEquals([new Product(['sku' => 'baz'])], $products->getProducts());
     }
 
-    public function testGetSizeWithOffsetEquals1()
+    public function testGetSizeWithOffsetEquals()
     {
         $products = new ProductCollection([new Product([])]);
         $products->limit(100);
         $this->assertEquals(1, $products->getSize());
     }
 
-    public function testGetSizeWithOffsetAndLimitEquals1()
+    public function testGetSizeWithOffsetAndLimitEquals()
     {
         $products = new ProductCollection([new Product([]), new Product([])]);
         $products->offset(2);
         $products->limit(1);
         $this->assertEquals(0, $products->getSize());
+
+        $products = new ProductCollection([new Product(['sku' => 1]), new Product(['sku' => 2]), new Product(['sku' => 3])]);
+        $products->offset(1);
+        $products->limit(1);
+        $this->assertEquals(1, $products->getSize());
     }
 
-    public function testGetProductsWithOffsetAndLimitEquals1()
+    public function testGetProductsWithOffsetAndLimitEquals()
     {
         $products = new ProductCollection([new Product(['sku' => 1]), new Product(['sku' => 2]), new Product(['sku' => 3])]);
         $products->offset(1);
         $products->limit(1);
         $this->assertEquals([new Product(['sku' => 2])], $products->getProducts());
-    }
-
-    public function testGetSizeWithOffsetAndLimitEquals2()
-    {
-        $products = new ProductCollection([new Product(['sku' => 1]), new Product(['sku' => 2]), new Product(['sku' => 3])]);
-        $products->offset(1);
-        $products->limit(1);
-        $this->assertEquals(1, $products->getSize());
     }
 }
--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -4,51 +4,39 @@ require_once __DIR__ . '/../src/Product.php';
 
 class ProductTest extends PHPUnit_Framework_TestCase
 {
-    public function testSkuEquals1()
+    public function testSkuEquals()
     {
         $product = new Product(['sku' => '12345']);
         $this->assertEquals('12345', $product->getSku());
-    }
 
-    public function testSkuEquals2()
-    {
         $product = new Product(['sku' => '567890']);
         $this->assertEquals('567890', $product->getSku());
     }
 
-    public function testNameEquals1()
+    public function testNameEquals()
     {
         $product = new Product(['name' => 'Nokio']);
         $this->assertEquals('Nokio', $product->getName());
-    }
 
-    public function testNameEquals2()
-    {
         $product = new Product(['name' => 'Motyorobla']);
         $this->assertEquals('Motyorobla', $product->getName());
     }
 
-    public function testImageEquals1()
+    public function testImageEquals()
     {
         $product = new Product(['image' => 'http://url.ru/img.jpg']);
         $this->assertEquals('http://url.ru/img.jpg', $product->getImage());
-    }
 
-    public function testImageEquals2()
-    {
         $product = new Product(['image' => 'http://url.ru/img2.jpg']);
         $this->assertEquals('http://url.ru/img2.jpg', $product->getImage());
     }
 
 
-    public function testPriceEquals1()
+    public function testPriceEquals()
     {
         $product = new Product(['price' => 123.5]);
         $this->assertEquals(123.5, $product->getPrice());
-    }
 
-    public function testPriceEquals2()
-    {
         $product = new Product(['price' => 42]);
         $this->assertEquals(42, $product->getPrice());
     }
@@ -65,14 +53,11 @@ class ProductTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(false, $product->isSpecialPriceApplied());
     }
 
-    public function testSpecialPriceEquals1()
+    public function testSpecialPriceEquals()
     {
         $product = new Product(['special_price' => 123.5]);
         $this->assertEquals(123.5, $product->getSpecialPrice());
-    }
 
-    public function testSpecialPriceEquals2()
-    {
         $product = new Product(['special_price' => 42]);
         $this->assertEquals(42, $product->getSpecialPrice());
     }
