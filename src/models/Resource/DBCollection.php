4ca550f420b349e64a8e8161dafd24839f250739
denis belov <deniswhite8@gmail.com>
2013-12-10 18:40:30 +0400

    fix bug in DBCollection

--- src/models/Resource/DBCollection.php
+++ src/models/Resource/DBCollection.php
@@ -11,6 +11,7 @@ class DBCollection
     private $_table;
     private $_filters = [];
     private $_select, $_driver, $_adapter, $_sql;
+    private $_limit, $_offset;
 
     public function __construct(\PDO $connection, Table\ITable $table)
     {
@@ -20,11 +21,12 @@ class DBCollection
         $this->_driver = new \Zend\Db\Adapter\Driver\Pdo\Pdo($this->_connection);
         $this->_adapter = new \Zend\Db\Adapter\Adapter($this->_driver);
         $this->_sql = new \Zend\Db\Sql\Sql($this->_adapter);
-        $this->_select = $this->_sql->select($this->_table->getName());
     }
 
     private function _executeSelect($columns = null)
     {
+        $this->_select = $this->_sql->select($this->_table->getName());
+
         if ($columns) {
             $this->_select->columns($columns);
         }
@@ -60,11 +62,27 @@ class DBCollection
         $this->_filters[$column] = $value;
     }
 
+    public function limit($limit)
+    {
+        $this->_limit = $limit;
+    }
+
+    public function offset($offset)
+    {
+        $this->_offset = $offset;
+    }
+
     private function _prepareFilters()
     {
         foreach ($this->_filters as $column => $value) {
 //            $this->_select->where("{$column} = :{$column}");
             $this->_select->where("{$column} = '{$value}'");
         }
+    }
+
+    private function _prepareLimit()
+    {
+        $this->_select->limit($this->_limit);
+        $this->_select->offset($this->_offset);
     }
 }
